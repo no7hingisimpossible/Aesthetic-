0d5ba77ffbccf2565609d317416164a1fc88f852
Fahim <fahim.madani@gmail.com>
2022-04-18 05:38:35 +0600

    googlesignin handled

--- src/Pages/GoogleSignIn/GoogleSignIn.js
+++ src/Pages/GoogleSignIn/GoogleSignIn.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { auth } from '../../firebase.init';
 import google from '../../images/google.png'
 import Loading from '../Loading/Loading';
@@ -9,9 +9,11 @@ import './GoogleSignIn.css'
 const GoogleSignIn = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
     const navigate = useNavigate()
+    const location = useLocation()
+    const from = location?.state?.from?.pathname || '/'
     useEffect(()=>{
        if(user){
-        navigate('/')
+        navigate(from, {replace: true})
        }
         
     },[user])
@@ -31,8 +33,8 @@ const GoogleSignIn = () => {
                     <img className='me-2' src={google} alt="" />
                     Continue with Google
                 </button>
-                {error?.message && <p>{error.message}</p>}
             </div>
+                {error?.message && <p className='text-center mt-2'>{error.message}</p>}
         </div>
     );
 };
